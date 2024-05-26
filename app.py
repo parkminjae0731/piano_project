ae0e86f60b43bd266e124a59e5990e254a5f6780
wonjun17234 <155791097+wonjun17234@users.noreply.github.com>
2024-05-26 16:01:19 +0900

    :memo:update

--- app.py
+++ app.py
@@ -8,6 +8,11 @@ coordinates = []
 staff = []
 pitch_name = str
 
+scaleStr = ["낮은 라", "낮은 시", "중간 도", "중간 레", 
+                "중간 미", "중간 파", "중간 솔", "중간 라", 
+                "중간 시", "높은 도", "높은 레", "높은 미",
+                "높은 파", "높은 솔", "높은 라", "높은 시", "더 높은 도"]
+
 @app.route('/', methods = ['POST','GET'])
 def index():
 
@@ -18,43 +23,48 @@ def clicked_coordinates():
     global pitch_name
     data = request.json
     print(data) # {'x': 242, 'y': -32}
-    x = data['x']
-    y = data['y']
+    idx = data['idx']
     
-    if y == 19:
-        pitch_name = "낮은 라"
-    elif y == 14:
-        pitch_name = "낮은 시"
-    elif y == 9:
-        pitch_name = "도"
-    elif y == 4:
-        pitch_name = "레"
-    elif y == -1:
-        pitch_name = "미"
-    elif y == -6:
-        pitch_name = "파"    
-    elif y == -11:
-        pitch_name = "솔"
-    elif y == -16:
-        pitch_name = "라"
-    elif y == -21:
-        pitch_name = "시"
-    elif y == -26:
-        pitch_name = "높은 도"
-    elif y == -31:
-        pitch_name = "높은 레"
-    elif y == -36:
-        pitch_name = "높은 미"
-    elif y == -41:
-        pitch_name = "높은 파"
-    elif y == -46:
-        pitch_name = "높은 솔"     
-    elif y == -51:
-        pitch_name = "높은 라"
-    elif y == -56:
-        pitch_name = "높은 시"
-    elif y == -61:
-        pitch_name = "더 높은 도"
+    x = 80 + 27 * len(staff)
+    y = 19 - idx * 5;
+    pitch_name = scaleStr[idx];
+
+
+    # if y == 19:
+    #     pitch_name = "낮은 라"
+    # elif y == 14:
+    #     pitch_name = "낮은 시"
+    # elif y == 9:
+    #     pitch_name = "도"
+    # elif y == 4:
+    #     pitch_name = "레"
+    # elif y == -1:
+    #     pitch_name = "미"
+    # elif y == -6:
+    #     pitch_name = "파"    
+    # elif y == -11:
+    #     pitch_name = "솔"
+    # elif y == -16:
+    #     pitch_name = "라"
+    # elif y == -21:
+    #     pitch_name = "시"
+    # elif y == -26:
+    #     pitch_name = "높은 도"
+    # elif y == -31:
+    #     pitch_name = "높은 레"
+    # elif y == -36:
+    #     pitch_name = "높은 미"
+    # elif y == -41:
+    #     pitch_name = "높은 파"
+    # elif y == -46:
+    #     pitch_name = "높은 솔"     
+    # elif y == -51:
+    #     pitch_name = "높은 라"
+    # elif y == -56:
+    #     pitch_name = "높은 시"
+    # elif y == -61:
+    #     pitch_name = "더 높은 도"
+
     print(pitch_name)
     coordinates.append((x, y))
     staff.append(((x, y), pitch_name))
@@ -62,17 +72,20 @@ def clicked_coordinates():
   
 @app.route('/print_coordinates', methods=['GET'])
 def print_coordinates():
-    sorted_staff = sorted(staff, key=lambda coord: coord[0][0])
+    sorted_staff = staff #sorted(staff, key=lambda coord: coord[0][0])
     a_staff = [coord[1] for coord in sorted_staff]
     print("Y 좌표:", [item[0][1] for item in staff])
     print(a_staff)
     print(staff)
+    mp3_files  = []
     for item in staff:
         coords, note = item
         x, y = coords
         print(x)
         print(y)
         print(note)
+        if note == '미':
+            mp3_files.append('sounds/scales/E3.mp3')
         
     return jsonify({"success": True, "message": str(a_staff)})
 
--- static/scripts/coordinate v.js
+++ static/scripts/coordinate v.js
@@ -1,4 +1,3 @@
-// 높은 음자리, 낮은 음자리 생성
 a = 0
 function sleep(ms) {
     const wakeUpTime = Date.now() + ms;
@@ -16,102 +15,31 @@ document.addEventListener("DOMContentLoaded", function () {
     staff2.appendChild(note);
 
 });
-
+scaleStr = ["낮은 라", "낮은 시", "중간 도", "중간 레", 
+                "중간 미", "중간 파", "중간 솔", "중간 라", 
+                "중간 시", "높은 도", "높은 레", "높은 미",
+                "높은 파", "높은 솔", "높은 라", "높은 시", "더 높은 도"];
 // 음표 생성
 coordinates = [];
 document.addEventListener("DOMContentLoaded", function () {
     var staff2 = document.querySelector(".staff2");
 
     staff2.addEventListener("click", function (event) {
+        console.log("Clicked!")
         var y = event.clientY - 60 - staff2.getBoundingClientRect().top;
         console.log(y)
         var x = 80 + a;
-        // x 좌표가 80보다 작으면 음표 생성하지 않음
-        if (x < 80) {
+        
+        if (x > 864) {
             return;
         }
-        else if (x > 864) {
-            return;
-        }
-        // y 좌표를 조건에 따라 설정
-        if (y >= 16.5 && y <= 100) {
-            y = 19; // 낮은라
-            console.log('낮은라')
-            var audio = new Audio('/static/sounds/scales/낮은 라.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= 11.5 && y <= 16.4) {
-            y = 14; // 낮은시
-            console.log('낮은시')
-            var audio = new Audio('/static/sounds/scales/낮은 시.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= 6.5 && y <= 11.4) {
-            y = 9; // 도
-            console.log('도')
 
-            // 도 음표를 재생하는 오디오 요소 추가
-            var audio = new Audio('/static/sounds/scales/중간 도.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= 1.5 && y <= 6.4) {
-            y = 4; // 레
-            console.log('레')
-            var audio = new Audio('/static/sounds/scales/중간 레.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -3.5 && y <= 1.4) {
-            y = -1; // 미
-            console.log('미')
-            var audio = new Audio('/static/sounds/scales/중간 미.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -8.5 && y <= -3.4) {
-            y = -6; // 파
-            console.log('파')
-            var audio = new Audio('/static/sounds/scales/중간 파.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -13.5 && y <= -8.4) {
-            y = -11; // 솔
-            console.log('솔')
-            var audio = new Audio('/static/sounds/scales/중간 솔.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -18.5 && y <= -13.4) {
-            y = -16; // 라
-            console.log('라')
-            var audio = new Audio('/static/sounds/scales/중간 라.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -23.5 && y <= -18.4) {
-            y = -21; // 시
-            console.log('시')
-            var audio = new Audio('/static/sounds/scales/중간 시.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -28.5 && y <= -23.4) {
-            y = -26; // 도
-            console.log('높은 도')
-            var audio = new Audio('/static/sounds/scales/높은 도.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -33.5 && y <= -28.4) {
-            y = -31; // 레
-            console.log('레')
-            var audio = new Audio('/static/sounds/scales/높은 레.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -38.5 && y <= -33.4) {
-            y = -36; // 미
-            console.log('미')
-            var audio = new Audio('/static/sounds/scales/높은 미.mp3'); // 경로 수정
-            audio.play();
-        } else if (y >= -43.5 && y <= -38.4) {
-            y = -41; // 파
-            console.log('파')
-        } else if (y >= -48.5 && y <= -43.4) {
-            y = -46; // 솔
-            console.log('솔')
-        } else if (y >= -53.5 && y <= -48.4) {
-            y = -51; // 라
-            console.log('라')
-        } else if (y >= -58.5 && y <= -53.4) {
-            y = -56; // 시
-            console.log('시')
-        } else if (y >= -100 && y <= -58.4) {
-            y = -61
-            console.log('짱 높은도')
-        }
+        if (y > 16.5) y = 16.5
+        var idx = Math.round(Math.abs(y - 16.5) / 5);
+        y = 19 - idx * 5;
+        console.log(scaleStr[idx]);
+        var audio = new Audio('/static/sounds/scales/' + scaleStr[idx] + '.mp3');
+        audio.play();
 
         // 클릭한 좌표를 서버로 전송
         fetch("/clicked_coordinates", {
@@ -119,29 +47,59 @@ document.addEventListener("DOMContentLoaded", function () {
             headers: {
                 "Content-Type": "application/json",
             },
-            body: JSON.stringify({ x: x, y: y }),
+            body: JSON.stringify({ idx: idx}),
         });
+                            // x y img width
+        var drawData = {"-51":[77, 42, "upper_removed", 24], 
+                        "-56":[75, 42, "upper_removed1", 26],
+                        "-61":[77, 42, "upper_removed2", 26],
+                        "-21":[80, 41, "m", 15],
+                        "-26":[80, 41, "m", 15],
+                        "-31":[80, 41, "m", 15],
+                        "-36":[80, 41, "m", 15],
+                        "-41":[80, 41, "m", 15],
+                        "-46":[80, 41, "m", 15],
+                        "9":[75, 12, "rm", 30],
+                        "14":[75, 5, "rm1", 30],
+                        "19":[75, 5, "rm2", 30],
+                        //"":[66, 12, "8min", 54]
+        };
 
         // 음표 생성
         var note = document.createElement("div");
         note.className = "note";
+        
+        yStr = y.toString()
+        console.log(yStr)
+
+        if (drawData[yStr] == undefined) {
+            y += 12
+            x = 66 + a
+            a += 27
+            note.innerHTML =
+                '<img src="/static/imagi/8min.png"  style="width: 54px; height: auto;">';
+        } else {
+            y += drawData[yStr][1]
+            x = drawData[yStr][0] + a
+            a += 27
+            note.innerHTML = '<img src="/static/imagi/' + drawData[yStr][2] + '.png"  style="width:' + drawData[yStr][3] + 'px; height: auto;">';
+              
+        }
+
+/*
         if (y == -51) {
             // y가 -51이면 rm.png 생성
             y += 42
-            x=80
-            x += a
+            x = 77 + a
             a += 27
-            x -= 3;
             note.innerHTML =
                 '<img src="/static/imagi/upper_removed.png"  style="width: 24px; height: auto;">';
          
         } else if (y == -56) {
             // y가 -56이면 m.png 생성
             y += 42
-            x=80
-            x += a
+            x = 75 + a
             a += 27
-            x -= 5;
             note.innerHTML =
                 '<img src="/static/imagi/upper_removed1.png"  style="width: 26px; height: auto;">';
 
@@ -149,68 +107,57 @@ document.addEventListener("DOMContentLoaded", function () {
         } else if (y == -61) {
             // y가 -61이면 m.png 생성
             y += 42
-            x=80
-            x += a
+            x = 77 + a
             a += 27
-            x -= 3;
             note.innerHTML =
                 '<img src="/static/imagi/upper_removed2.png"  style="width: 26px; height: auto;">';
         } else if (y <= -21) {
             // y가 -21보다 작으면 m.png 생성
             y += 41;
-            x=80
-            x += a
+            x = 80 + a
             a += 27
             note.innerHTML =
                 '<img src="/static/imagi/m.png"  style="width: 15px; height: auto;">';
         } else if (y == 9) {
             // y가 -61이면 m.png 생성
             y += 12
-            x=80
-            x += a
+            x = 75 + a
             a += 27
-            x -= 5
             note.innerHTML =
                 '<img src="/static/imagi/rm.png"  style="width: 30px; height: auto;">';
         } else if (y == 14) {
             // y가 -61이면 m.png 생성
             y += 5
-            x=80
-            x += a
+            x = 75 + a
             a += 27
-            x -= 5
             note.innerHTML =
                 '<img src="/static/imagi/rm1.png"  style="width: 30px; height: auto;">';
         } else if (y == 19) {
             // y가 -61이면 m.png 생성
             y += 5
-            x=80
-            x += a
+            x = 75 + a
             a += 27
-            x -= 5
             note.innerHTML =
                 '<img src="/static/imagi/rm2.png"  style="width: 30px; height: auto;">';
 
         } else {
             // 그 외에는 음표 생성
             y += 12
-            x=80
-            x += a
+            x = 66 + a
             a += 27
-            x -= 14
             note.innerHTML =
                 '<img src="/static/imagi/8min.png"  style="width: 54px; height: auto;">';
 
             //♩
         }
-        
+        */
         note.style.position = "absolute";
         note.style.left = x + "px";
         note.style.top = y + "px";
         staff2.appendChild(note);
 
         // 클릭한 좌표들을 저장하는 배열에 추가
-        coordinates.push({ x: x, y: y });
+        coordinates.push({ idx : idx});
     });
     window.addEventListener('beforeunload', function () {
         fetch('/reset_coordinates', {
@@ -221,104 +168,35 @@ document.addEventListener("DOMContentLoaded", function () {
     var printButton = document.getElementById("printButton");
     printButton.addEventListener("click", function () {
         // 좌표를 x 좌표 기준 오름차순 정렬
-        coordinates.sort(function (a, b) {
-            return a.x - b.x;
-        });
+        // coordinates.sort(function (a, b) {
+        //     return a.x - b.x;
+        // });
         console.log(coordinates)
         // 정렬된 좌표 출력
         coordinates.forEach(function (coord) {
-            console.log("Y 좌표:", coord.y);
+            console.log("Y 좌표:", 19 - coord.idx * 5);
             
         });
         
-        for(let i=80;i<860;i++){
-            coordinates.forEach(function (coord) {
-                if (coord.x === i){
-                    y = coord.y
-                    sleep(1000);
-                    if (y >= 16.5 && y <= 100) {
-                        y = 19; // 낮은라
-                        console.log('낮은라')
-                        var audio = new Audio('/static/sounds/scales/낮은 라.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= 11.5 && y <= 16.4) {
-                        y = 14; // 낮은시
-                        console.log('낮은시')
-                        var audio = new Audio('/static/sounds/scales/낮은 시.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= 6.5 && y <= 11.4) {
-                        y = 9; // 도
-                        console.log('도')
-            
-                        // 도 음표를 재생하는 오디오 요소 추가
-                        var audio = new Audio('/static/sounds/scales/중간 도.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= 1.5 && y <= 6.4) {
-                        y = 4; // 레
-                        console.log('레')
-                        var audio = new Audio('/static/sounds/scales/중간 레.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -3.5 && y <= 1.4) {
-                        y = -1; // 미
-                        console.log('미')
-                        var audio = new Audio('/static/sounds/scales/중간 미.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -8.5 && y <= -3.4) {
-                        y = -6; // 파
-                        console.log('파')
-                        var audio = new Audio('/static/sounds/scales/중간 파.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -13.5 && y <= -8.4) {
-                        y = -11; // 솔
-                        console.log('솔')
-                        var audio = new Audio('/static/sounds/scales/중간 솔.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -18.5 && y <= -13.4) {
-                        y = -16; // 라
-                        console.log('라')
-                        var audio = new Audio('/static/sounds/scales/중간 라.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -23.5 && y <= -18.4) {
-                        y = -21; // 시
-                        console.log('시')
-                        var audio = new Audio('/static/sounds/scales/중간 시.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -28.5 && y <= -23.4) {
-                        y = -26; // 도
-                        console.log('높은 도')
-                        var audio = new Audio('/static/sounds/scales/높은 도.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -33.5 && y <= -28.4) {
-                        y = -31; // 레
-                        console.log('레')
-                        var audio = new Audio('/static/sounds/scales/높은 레.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -38.5 && y <= -33.4) {
-                        y = -36; // 미
-                        console.log('미')
-                        var audio = new Audio('/static/sounds/scales/높은 미.mp3'); // 경로 수정
-                        audio.play();
-                    } else if (y >= -43.5 && y <= -38.4) {
-                        y = -41; // 파
-                        console.log('파')
-                    } else if (y >= -48.5 && y <= -43.4) {
-                        y = -46; // 솔
-                        console.log('솔')
-                    } else if (y >= -53.5 && y <= -48.4) {
-                        y = -51; // 라
-                        console.log('라')
-                    } else if (y >= -58.5 && y <= -53.4) {
-                        y = -56; // 시
-                        console.log('시')
-                    } else if (y >= -100 && y <= -58.4) {
-                        y = -61
-                        console.log('짱 높은도')
-                    }
-                }
-                
-            });
-           
+        function playSound(idx) {
+            y = 19 - idx * 5;
+            console.log(scaleStr[idx]);
+            var audio = new Audio('/static/sounds/scales/' + scaleStr[idx] + '.mp3');
+            audio.play();
         }
+        
+        async function sleep(ms) {
+            return new Promise(resolve => setTimeout(resolve, ms));
+        }
+        
+        async function processCoordinates() {
+            for (const coord of coordinates) {
+                await sleep(1500);
+                playSound(coord.idx);
+            }
+        }
+        
+        processCoordinates();
 
         
         alert("좌표를 성공적으로 받아왔습니다.");
@@ -349,9 +227,6 @@ document.addEventListener("DOMContentLoaded", function () {
 function addHighNoteAutomatically() {
    var staff2 = document.querySelector(".staff2");
    console.log(staff2)
-   
-   
-
        var note = document.createElement("div");
        note.className = "note";
        note.innerHTML =
